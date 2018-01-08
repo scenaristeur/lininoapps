047af5598440707ee69e9b7ce3c2c344a4897171
unknown <scenaristeur@gmail.com>
2018-01-09 00:28:28 +0100

    update pot2web

---
+++ root/pot2web/tutoriel/pot2web/index.js
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////
+// GESTION NODE EXPRESS SERVEUR
+///////////////////////////////////////////////////////////
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3005;
+
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+//LANCEMENT SERVEUR
+server.listen(port, function () {
+	console.log('Server listening at port %d', port);
+});
